6ed9625139dbb664e789c56f821837a688373687
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-03-11 10:52:28 -0700

    updated api key validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
 class ApplicationController < ActionController::API
-    before_action: :authorized
+    before_action :authorized
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def api_key
+        if auth_header
+            api_key = auth_header.split(' ')[1]
+        end
+    end
+
+    def validate_api_key
+        if api_key
+            valid_key = ApiUser.find_by(api_key: api_key)
+            # valid_key = 'yrw_Ae6VGv4kqvwvozfn9g'
+        end
+    end
 
     def is_valid?
-
+        !!validate_api_key
     end
 
     def authorized
