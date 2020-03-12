adf71621e4793c6a514759d443c51500ecb69333
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-03-12 11:14:19 -0700

    created transaction#create

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -8,6 +8,17 @@ class Api::V1::TransactionsController < ApplicationController
         end
     end
 
+    def create
+        transaction = Transaction.new(transaction_params)
+        if transaction.save
+            render json: transaction, status: :created
+        elsif !transaction.valid?
+            render json: { message: transaction.errors }
+        else
+            render json: { message: 'There was an error processing your request, please try again later.' }, status: :bad_request
+        end
+    end
+
     private
 
     def transaction_params
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :coin
   belongs_to :api_user
+
+  validates :transaction_type, presence: true, inclusion: { in: %w(deposit withdrawal), message: "%{value} is not an accepted transaction type. Must be either deposit or withdrawalˀ" }
+  validates :coin_id, presence: true
+  validates :api_user_id, presence: true
 end
