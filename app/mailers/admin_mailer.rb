258582d74cdd30d567b92e9f3d76c582d090e681
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-03-13 10:06:56 -0700

    wrote email for low-coin notification, testing now..

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,10 +1,10 @@
-class AdminMailer < ApplicationMailer
+sclass AdminMailer < ApplicationMailer
     default to: -> { Admin.pluck(:email) },
             from: 'notifications@coin-machine-api.com'
 
     def low_coin_email(coin)
         @coin = coin
         @total_value = Coin.total_coin_value
-        
+        mail(subject: "#{@coin.name}s are running low")
     end
 end
